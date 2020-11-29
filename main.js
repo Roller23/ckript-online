b755c97fffe2a4ff856e8d2cc8c51afe426de892
Wiktor Kania <roller47@wp.pl>
2020-11-29 18:59:29 +0100

    Automated Makefile commit

--- main.js
+++ main.js
@@ -150,8 +150,5 @@
 
   let saveCodeInterval = setInterval(() => {
     localStorage.lastCode = editor.getValue();
-    console.log(CodeMirror.hint.anyword(editor, {
-      word: new RegExp("\\b" + "test" + "\\b")
-    }))
   }, 1000);
 })();
