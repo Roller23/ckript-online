c99bd1aa9f9cd67b217c73990424eb4be11016be
Wiktor Kania <roller47@wp.pl>
2020-11-29 13:33:32 +0100

    Automated Makefile commit

--- editor/lib/codemirror.js
+++ editor/lib/codemirror.js
@@ -71,6 +71,7 @@
   function elt(tag, content, className, style) {
     var e = document.createElement(tag);
     if (className) { e.className = className; }
+    console.log('creating', className)
     if (style) { e.style.cssText = style; }
     if (typeof content == "string") { e.appendChild(document.createTextNode(content)); }
     else if (content) { for (var i = 0; i < content.length; ++i) { e.appendChild(content[i]); } }
