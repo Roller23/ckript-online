f0be83b7db27176d022ae322042dcf11eac67af3
Wiktor Kania <roller47@wp.pl>
2020-11-29 04:38:29 +0100

    Automated Makefile commit

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 (function() {
   let output = document.querySelector('.output');
   let worker = new Worker('worker.js');
-  Module.printCallback = function(text) {
-    // let div = document.createElement('div');
-    // div.innerText = text;
-    // setTimeout(() => output.appendChild(div), 0);
+  // Module.printCallback = function(text) {
+  //   // let div = document.createElement('div');
+  //   // div.innerText = text;
+  //   // setTimeout(() => output.appendChild(div), 0);
 
-  }
+  // }
   document.querySelector('button').addEventListener('click', e => {
     document.querySelector('.output').innerHTML = '';
     let code = document.querySelector('textarea').value;
