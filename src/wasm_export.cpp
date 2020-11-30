e399d398db7f6a858956f6cc1f5266b12eb56c6e
Wiktor Kania <roller47@wp.pl>
2020-11-30 13:42:29 +0100

    Automated Makefile commit

--- src/wasm_export.cpp
+++ src/wasm_export.cpp
@@ -1,11 +1,19 @@
 #include "interpreter.hpp"
 
+#include <pthread.h>
+
 // this is the "main" file
 // runCode is exposed to Javascript and can be later called from there
 // __attribute__((used)) needed so the compiler doesn't optimize out the function
 
+void *run(void *arg) {
+  Interpreter().process_string((char *)arg);
+  return NULL;
+}
+
 extern "C" {
-  void __attribute__((used)) runCode(const char *code) {
-    Interpreter().process_string(code);
+  void __attribute__((used)) runCode(char *code) {
+    pthread_t thread;
+    pthread_create(&thread, NULL, run, code);
   }
 }
