bb008781eb21187fa85daccd90e2146211921be3
Wiktor Kania <roller47@wp.pl>
2023-01-14 00:07:12 +0100

    add alloc example

--- main.js
+++ main.js
@@ -102,8 +102,17 @@
 
   println(preallocated);
 
-  const str mem1 = "Hello";
-  const str mem2 = "World";
+  // memory allocation
+
+  alloc int mem1 = 5; // 5 is allocated on the heap and 'mem1' is a pointer to that location
+  ref int mem2 = mem1; // 'mem2' points to the 'mem1' location
+
+  mem2 += 8; // dereferencing is implicit
+
+  println('mem1 is @1, mem2 is @2'(mem1, mem2)); // string interpolation
+
+  // Functions can accept refs, arrays can hold refs
+
   println('mem1 is @1, mem2 is @2'(mem1, mem2)); // string interpolation
 
   // Classes and objects
